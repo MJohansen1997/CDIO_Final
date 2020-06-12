6341bc345037f8293e526ce1ce0ec9e1887be4eb
Boosted-Sama <s195485@student.dtu.dk>
2020-06-12 13:45:42 +0200

    Råvare DAO og DTO Made - Nikolai

--- src/main/java/DTO/RaavareDTO.java
+++ src/main/java/DTO/RaavareDTO.java
@@ -1,18 +1,44 @@
 package DTO;
 
-public class RaavareDTO {
-    /**
-     * raavare id i området 1-99999999 vælges af brugerne
-     */
-    String raavareID;
-    /**
-     * min. 2 max. 20 karakterer
-     */
-    String raavareNavn;
-    /**
-     * min. 2 max. 20 karakterer
-     */
-    String leverandoer;
-}
+public class RaavareDTO
+{
+    public RaavareDTO(String rID, String rN, String lev) {
+        raavareID = rID;
+        raavareNavn = rN;
+        leverandoer = lev;
+    }
 
+    //raavare id i området 1-99999999 vælges af brugerne
+    String raavareID;
+
+    // min. 2 max. 20 karakterer
+    String raavareNavn;
+
+    // min. 2 max. 20 karakterer
+    String leverandoer;
+
+    public String getRaavareID() {
+        return raavareID;
+    }
+
+    public void setRaavareID(String raavareID) {
+        this.raavareID = raavareID;
+    }
+
+    public String getRaavareNavn() {
+        return raavareNavn;
+    }
+
+    public void setRaavareNavn(String raavareNavn) {
+        this.raavareNavn = raavareNavn;
+    }
+
+    public String getLeverandoer() {
+        return leverandoer;
+    }
+
+    public void setLeverandoer(String leverandoer) {
+        this.leverandoer = leverandoer;
+    }
+}
 
