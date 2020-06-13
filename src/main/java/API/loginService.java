8978ecf941613613991c86cd44833c4797855165
Pope Morris <s175194@student.dtu.dk>
2020-06-13 18:20:03 +0200

    Første API forsøg*

--- src/main/java/API/loginService.java
+++ src/main/java/API/loginService.java
@@ -5,7 +5,6 @@ import DAO.DALException;
 import DTO.BrugerDTO;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import java.sql.SQLException;
 
 @Path("/login")
@@ -13,10 +12,8 @@ public class loginService{
 
     @POST
     @Path("/verify")
-    public boolean verification(
-            @FormParam("Username") String name,
-            @FormParam("Password") String password
-    ) throws SQLException, ClassNotFoundException, DALException {
+    public boolean verification(@FormParam("Username") String name, @FormParam("Password") String password)
+            throws SQLException, ClassNotFoundException, DALException {
         BrugerDAO BDAO = new BrugerDAO();
         //BrugerDTO user = BDAO.getBruger(name);
         BrugerDTO user = new BrugerDTO("B0001", "mikkel","MJ" ,"000000-0000", "0000");
