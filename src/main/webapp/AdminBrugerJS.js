7fd3b3b59e120e0c747e00d968b92b354700be66
Jacob <s174130@student.dtu.dk>
2020-06-16 13:14:20 +0200

    Changed AdminBrugerJS so buttons creates a form

--- src/main/webapp/AdminBrugerJS.js
+++ src/main/webapp/AdminBrugerJS.js
@@ -52,7 +52,7 @@ function loadBrugere() {
 }
 
 function generateHTML(bruger){
- return '<tr><td>' + bruger.brugerID + '</td>' +
+        return '<tr><td>' + bruger.brugerID + '</td>' +
         '<td>' + bruger.brugerNavn + '</td>' +
         '<td>' + bruger.ini + '</td>' +
         '<td>' + bruger.cpr + '</td>' +
@@ -61,93 +61,34 @@ function generateHTML(bruger){
     //@TODO Make a button which creates, updates and remove
 }
 
-jQuery(document).ready(function() {
-    $("#formButton").click(function() {
-        $("#form1").toggle();
+
+jQuery(document).ready(function () {
+    $("#buttonOpret").click(function () {
+        $("#opretForm").toggle();
     });
 });
 
-
-$("#buttonOpret").click(function(){
-    $("#modal1").append(generateMyOpretHtml());
+jQuery(document).ready(function () {
+    $("#buttonRediger").click(function () {
+        $("#redigerForm").toggle();
+    });
 });
 
-$("#buttonRediger").click(function(){
-    $(".modal2").append(("" +
-        "<span class=\"close2\">&times;</span>\n" +
-        "    <form action=\"rest/login/admin\" method=\"PUT\" id='adminrediger'>\n" +
-        "        <p>Vælg userid : <input type=\"text\" id=\"userid\" name=\"userid\" required/></p>\n" +
-        "    </form>\n" +
-        "    <button>Gem</button>"))});
-
-
-$("#buttonSlet").click(function(){
-    $("modal3").append("" +
-        "<span class=\"close\">&times;</span>\n" +
-        "    <form action=\"rest/login/admin\" method=\"GET\" id='adminslet'>\n" +
-        "        <p>Vælg userid : <input type=\"text\" id=\"userid\" name=\"userid\" required/></p>\n" +
-        "    </form>\n" +
-        "    <button>Gem</button>")});
-
-$("#buttonFind").click(function(){
-    $("modal4").append("" +
-        "<span class=\"close\">&times;</span>\n" +
-        "    <form action=\"rest/login/admin\" method=\"GET\" id='adminfind'>\n" +
-        "        <p>Vælg userid : <input type=\"text\" id=\"userid\" name=\"userid\" required/></p>\n" +
-        "    </form>\n" +
-        "    <button>Gem</button>")});
-
-
-
-
-
-
-
-<!--pop up opret-->
-var modal1 = document.getElementById("#opretForm");
-var opret = document.getElementById("#buttonOpret");
-var span = document.getElementsByClassName("close")[0];
-
-jQuery(opret).click(function() {
-    modal1.style.display = "block";
-});
-jQuery(span).click(function() {
-    modal1.style.display = "none";
+jQuery(document).ready(function () {
+    $("#buttonSlet").click(function () {
+        $("#sletForm").toggle();
+    });
 });
 
-    <!--pop up rediger-->
-var modal2 = document.getElementById("rediger");
-var rediger = document.getElementById("buttonRediger");
-
-jQuery(rediger).click(function() {
-    modal2.style.display = "block";
+jQuery(document).ready(function () {
+    $("#buttonFind").click(function () {
+        $("#findForm").toggle();
+    });
 });
 
-jQuery(span).click(function() {
-    modal2.style.display = "none";
-});
-
-
-    <!--pop up find-->
-var modal3 = document.getElementById("find");
-var find = document.getElementById("buttonFind");
-
-find.onclick = function() {
-    modal3.style.display = "block";
-};
-span.onclick = function() {
-    modal3.style.display = "none";
-};
-
-
-    <!--pop up slet-->
-var modal4 = document.getElementById("slet");
-var slet = document.getElementById("buttonSlet");
-
-slet.onclick = function() {
-    modal4.style.display = "block";
-};
-span.onclick = function() {
-    modal4.style.display = "none";
-};
+jQuery(document).ready(function () {
+    $("#submit4").click(function () {
+        $("#showForm").toggle();
+    });
+})
 
